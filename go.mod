ceceda2e650fbaaf7947d59811e290c682a7bbe7
David <rynstheoverlord@gmail.com>
2025-05-18 10:12:08 +0100

    Nothing changed, just for the name update

---
+++ go.mod
@@ -0,0 +1,14 @@
+module ripple
+
+go 1.24.2
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/term v0.32.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/manifoldco/promptui"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	clearScreen()
+	fmt.Println("Looking for internal storage...")
+	var items = []string{"Deep search", "Create task"}
+	hasMoved := moveToHomeDir()
+
+	current, err := os.Getwd()
+	if err != nil {
+		color.Red("Unable to switch paths")
+		fmt.Println("Is this termux?")
+		return
+	}
+
+	time.Sleep(time.Second)
+	if hasMoved {
+		fmt.Printf("Current: %v\n", current)
+	} else {
+		fmt.Println("Failed!")
+		return
+	}
+
+	if !findDir(current + "/storage") {
+		color.Red("Storage path not found!")
+		fmt.Println("Termux doesen't have access to internal storage.")
+		fmt.Println("Use termux-setup-storage to give it access.")
+		fmt.Println("Exiting process!")
+		return
+	}
+
+	time.Sleep(time.Second)
+	color.Green("Successfully accessed Internal storage!")
+	fmt.Println("Loading commands...")
+	time.Sleep(time.Second / 2)
+
+	command := promptui.Select{
+		Label: "Commands",
+		Items: items,
+	}
+
+	_, res, err := command.Run()
+
+	if err != nil {
+		color.Yellow("Application exited!")
+		fmt.Println("Was this intentional?")
+		return
+	}
+
+	if res == items[0] {
+		deepSearchCommandCenter(current + "/storage")
+	}
+
+}
+
+// the deep search command center
+func deepSearchCommandCenter(path string) {
+	clearScreen()
+	var prompt = promptui.Prompt{
+		Label: "Enter file name or extension to find",
+	}
+
+	res, err := prompt.Run()
+
+	if err != nil {
+		return
+	}
+
+	clearScreen()
+	fmt.Printf("Looking for %v in storage [%v]\n", res, path)
+	var exists, fpath = searchPath(path, res)
+	if exists {
+		color.Green("File found at : %v\n", fpath)
+	} else {
+		color.Red("Unable to find file!")
+	}
+}
+
+func moveToHomeDir() bool {
+	home, err := os.UserHomeDir()
+
+	if err != nil {
+		color.Red("Error: Could not find the home directory!")
+		fmt.Println("Exiting process...")
+
+		return false
+	}
+
+	var err2 = os.Chdir(home)
+	if err2 != nil {
+		color.Red("Failed to change directory!")
+		fmt.Println("Ripple is unable to switch to the home directory!")
+		fmt.Println("Exiting process...")
+		return false
+	}
+
+	return true
+}
+
+// searches a specified path
+func searchPath(path string, filename string) (bool, string) {
+	fmt.Printf("Search path: %v\n", path)
+	files, err := os.ReadDir(path)
+	if err != nil {
+		color.Red("Failed to open directory!")
+		return false, ""
+	}
+
+	for _, entry := range files {
+		fullpath := filepath.Join(path, entry.Name())
+		info, err := os.Stat(fullpath)
+
+		if err != nil {
+			color.Red("Failed to grab stat!")
+			return false, ""
+		}
+		if info.IsDir() {
+			fmt.Println("Opening new dir")
+			found, fullpath := searchPath(filepath.Join(path, entry.Name()), filename)
+			if found {
+				return true, fullpath
+			}
+		} else if entry.Name() == filename {
+			return true, filepath.Join(path, filename)
+		}
+	}
+	return false, ""
+
+}
+
+// follow a system link
+func resolveSymLink(fullpath string) (string, error) {
+	target, err := os.Readlink(fullpath)
+
+	if err != nil {
+		return "", err
+	}
+
+	if !filepath.IsAbs(fullpath) {
+		target = filepath.Join(filepath.Dir(fullpath), target)
+	}
+
+	return target, nil
+}
+
+// clears the terminal screen
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
+}
+
+func findDir(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
